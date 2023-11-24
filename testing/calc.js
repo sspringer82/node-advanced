6bf4c593c1895e5fc53f715cfd85dfa2325c8c8c
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2023-11-24 11:58:29 +0100

    Aufgabe 11 Lösung

--- testing/calc.js
+++ testing/calc.js
@@ -2,4 +2,11 @@ export default class Calc {
   add(a, b) {
     return a + b;
   }
+
+  multi(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+      throw new Error('not a number');
+    }
+    return a * b;
+  }
 }
--- testing/calc.test.js
+++ testing/calc.test.js
@@ -20,23 +20,51 @@ describe('Calc', () => {
   afterEach(() => {});
   afterAll(() => {});
 
-  it('should add 1 and 1 and return 2', () => {
-    // Act
-    const result = calc.add(1, 1);
+  describe('.add', () => {
+    it('should add 1 and 1 and return 2', () => {
+      // Act
+      const result = calc.add(1, 1);
 
-    // Assert
-    expect(result).toBe(2);
+      // Assert
+      expect(result).toBe(2);
+    });
+
+    it('should add 2 and 3 and return 5', () => {
+      // Act
+      const result = calc.add(2, 3);
+
+      // Assert
+      expect(result).toBe(5);
+    });
+
+    it('should add 1 and 1 and return 2', () => {
+      expect(new Calc().add(1, 1)).toBe(2);
+    });
   });
 
-  it('should add 2 and 3 and return 5', () => {
-    // Act
-    const result = calc.add(2, 3);
+  describe('.multi', () => {
+    it('should multiply 2 with 3 and return 6', () => {
+      const result = calc.multi(2, 3);
 
-    // Assert
-    expect(result).toBe(5);
-  });
+      expect(result).toBe(6);
+    });
 
-  it('should add 1 and 1 and return 2', () => {
-    expect(new Calc().add(1, 1)).toBe(2);
+    it('should throw an exception if the first parameter is not a number', () => {
+      const testFn = () => calc.multi('1', 2);
+
+      expect(testFn).toThrow();
+    });
+
+    it('should throw an exception if the second parameter is not a number', () => {
+      const testFn = () => calc.multi(1, '2');
+
+      expect(testFn).toThrow();
+    });
+
+    it('should throw an exception if both parameters are not a number', () => {
+      const testFn = () => calc.multi('1', '2');
+
+      expect(testFn).toThrow();
+    });
   });
 });
