c08fec07ad47f1837ee0df1c86fd743c60979aec
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2023-11-24 13:29:34 +0100

    Aufgabe 12 Lösung

--- Aufgaben/Aufgabe12.md
+++ Aufgaben/Aufgabe12.md
@@ -2,4 +2,4 @@
 
 Teste den Fehlerfall in async.test.js
 
-Ein Aufruf von doSomethingAsync(() => {}, true) soll einen Fehler verusachen. Das erste Argument der Callback-Funktion darf nicht Null sein, sondern musse in Fehlerobjekt sein.
+Ein Aufruf von doSomethingAsync(() => {}, true) soll einen Fehler verusachen. Das erste Argument der Callback-Funktion darf nicht Null sein, sondern muss ein Fehlerobjekt sein.
--- testing/async.test.js
+++ testing/async.test.js
@@ -11,4 +11,34 @@ describe('doSomethingAsync', () => {
 
     expect(promise).resolves.toEqual([null, 'Hallo Welt']);
   });
+
+  it('should return "Hallo Welt"', () => {
+    const promise = new Promise((resolve, reject) => {
+      doSomethingAsync((error, data) => {
+        resolve(error);
+      });
+    });
+
+    expect(promise).resolves.toEqual(null);
+  });
+
+  it('should return "Hallo Welt"', () => {
+    const promise = new Promise((resolve, reject) => {
+      doSomethingAsync((error, data) => {
+        resolve(data);
+      });
+    });
+
+    expect(promise).resolves.toEqual('Hallo Welt');
+  });
+
+  it('should get an error', () => {
+    const promise = new Promise((resolve, reject) => {
+      doSomethingAsync((error, data) => {
+        resolve([error, data]);
+      }, true);
+    });
+
+    expect(promise).resolves.toEqual([new Error('whoops'), undefined]);
+  });
 });
